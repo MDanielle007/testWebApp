e0ebb5626764fd34b947070212d1bdab3f6e0175
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2023-11-03 13:50:07 +0800

    Initial CI4 commit

---
+++ app/Models/FarmerProfilesModel.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class FarmerProfilesModel extends Model
+{
+    protected $table            = 'farmer_profile';
+    protected $primaryKey       = 'Farmer_ID';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [ 'User_ID', 'Years_Of_Farming', 'Record_Status'];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+}
